9b4266329b325a0b49e19e9c40b61b4e678fa514
jwd-dev <jake@jdocs.app>
2023-04-23 11:51:24 -0400

    init commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from "@mui/material/Button";
+import SignIn from "./signin";
+import Dashboard from "./dash";
+
+const App = () => {
+
+
+  return (
+      <div>
+      <Dashboard/>
+      </div>
+  );
+};
+
+export default App;
